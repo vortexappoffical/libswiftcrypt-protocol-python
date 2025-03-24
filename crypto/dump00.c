67947dd53e31bc1dade50f8d7552c8391bc27b03
vortexappoffical <agneskeita@gmail.com>
2025-03-24 09:35:19 +0100

    Update dump00.c

--- crypto/dump00.c
+++ crypto/dump00.c
@@ -1,223 +1,176 @@
 #include <Python.h>
 #include <openssl/aes.h>
 #include <openssl/rand.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define KEY_SIZE 32 // 256 bits
-#define BLOCK_SIZE 16 // AES block size
+#define AES_KEY_LENGTH 256
+#define AES_BLOCK_SIZE 16
 
-static void handleErrors(void)
-{
-    ERR_print_errors_fp(stderr);
-    abort();
-}
-
-static int generate_key(unsigned char* key) {
-    if (!RAND_bytes(key, KEY_SIZE)) {
-        handleErrors();
-        return 0;
+static PyObject *generate_key(PyObject *self, PyObject *args) {
+    unsigned char key[AES_KEY_LENGTH/8];
+    if (!RAND_bytes(key, sizeof(key))) {
+        PyErr_SetString(PyExc_RuntimeError, "Failed to generate key");
+        return NULL;
     }
-    return 1;
+    return PyBytes_FromStringAndSize((const char *)key, sizeof(key));
 }
 
-static int encrypt_ige(const unsigned char* plaintext, int plaintext_len, const unsigned char* key, unsigned char* ciphertext) {
+static PyObject *encrypt_data(PyObject *self, PyObject *args, int is_metadata) {
+    const char *data;
+    unsigned char key[AES_KEY_LENGTH/8];
+    unsigned char iv[AES_BLOCK_SIZE];
+    int data_len;
+    if (!PyArg_ParseTuple(args, "s#y#y#", &data, &data_len, &key, sizeof(key), &iv, sizeof(iv))) {
+        return NULL;
+    }
+
+    int encrypted_len = ((data_len + AES_BLOCK_SIZE) / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
+    unsigned char *encrypted = malloc(encrypted_len);
+    if (!encrypted) {
+        PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory for encryption");
+        return NULL;
+    }
+
     AES_KEY aes_key;
-    unsigned char iv[BLOCK_SIZE * 2];
+    AES_set_encrypt_key(key, AES_KEY_LENGTH, &aes_key);
+    AES_ige_encrypt((unsigned char *)data, encrypted, data_len, &aes_key, iv, AES_ENCRYPT);
 
-    if (AES_set_encrypt_key(key, 256, &aes_key) < 0) {
-        handleErrors();
-        return 0;
-    }
-
-    if (!RAND_bytes(iv, BLOCK_SIZE * 2)) {
-        handleErrors();
-        return 0;
-    }
-
-    AES_ige_encrypt(plaintext, ciphertext, plaintext_len, &aes_key, iv, AES_ENCRYPT);
-    return 1;
+    PyObject *result = PyBytes_FromStringAndSize((const char *)encrypted, encrypted_len);
+    free(encrypted);
+    return result;
 }
 
-static int decrypt_ige(const unsigned char* ciphertext, int ciphertext_len, const unsigned char* key, unsigned char* plaintext) {
+static PyObject *decrypt_data(PyObject *self, PyObject *args, int is_metadata) {
+    const unsigned char *data;
+    unsigned char key[AES_KEY_LENGTH/8];
+    unsigned char iv[AES_BLOCK_SIZE];
+    int data_len;
+    if (!PyArg_ParseTuple(args, "y#y#y#", &data, &data_len, &key, sizeof(key), &iv, sizeof(iv))) {
+        return NULL;
+    }
+
+    unsigned char *decrypted = malloc(data_len);
+    if (!decrypted) {
+        PyErr_SetString(PyExc_MemoryError, "Failed to allocate memory for decryption");
+        return NULL;
+    }
+
     AES_KEY aes_key;
-    unsigned char iv[BLOCK_SIZE * 2];
+    AES_set_decrypt_key(key, AES_KEY_LENGTH, &aes_key);
+    AES_ige_encrypt(data, decrypted, data_len, &aes_key, iv, AES_DECRYPT);
 
-    if (AES_set_decrypt_key(key, 256, &aes_key) < 0) {
-        handleErrors();
-        return 0;
-    }
-
-    if (!RAND_bytes(iv, BLOCK_SIZE * 2)) {
-        handleErrors();
-        return 0;
-    }
-
-    AES_ige_encrypt(ciphertext, plaintext, ciphertext_len, &aes_key, iv, AES_DECRYPT);
-    return 1;
+    PyObject *result = PyBytes_FromStringAndSize((const char *)decrypted, data_len);
+    free(decrypted);
+    return result;
 }
 
-static PyObject* py_generate_key(PyObject* self, PyObject* args) {
-    unsigned char key[KEY_SIZE];
-    if (!generate_key(key)) {
-        return NULL;
-    }
-    return Py_BuildValue("y#", key, KEY_SIZE);
+static PyObject *encrypt_text(PyObject *self, PyObject *args) {
+    return encrypt_data(self, args, 0);
 }
 
-static PyObject* py_encrypt(PyObject* self, PyObject* args) {
-    const char* plaintext;
-    const char* key;
-    int plaintext_len, key_len;
-
-    if (!PyArg_ParseTuple(args, "y#y#", &plaintext, &plaintext_len, &key, &key_len)) {
-        return NULL;
-    }
-
-    unsigned char ciphertext[plaintext_len + BLOCK_SIZE];
-    if (!encrypt_ige((unsigned char*)plaintext, plaintext_len, (unsigned char*)key, ciphertext)) {
-        return NULL;
-    }
-
-    return Py_BuildValue("y#", ciphertext, plaintext_len + BLOCK_SIZE);
+static PyObject *decrypt_text(PyObject *self, PyObject *args) {
+    return decrypt_data(self, args, 0);
 }
 
-static PyObject* py_decrypt(PyObject* self, PyObject* args) {
-    const char* ciphertext;
-    const char* key;
-    int ciphertext_len, key_len;
-
-    if (!PyArg_ParseTuple(args, "y#y#", &ciphertext, &ciphertext_len, &key, &key_len)) {
-        return NULL;
-    }
-
-    unsigned char plaintext[ciphertext_len];
-    if (!decrypt_ige((unsigned char*)ciphertext, ciphertext_len, (unsigned char*)key, plaintext)) {
-        return NULL;
-    }
-
-    return Py_BuildValue("y#", plaintext, ciphertext_len);
+static PyObject *encrypt_metadata(PyObject *self, PyObject *args) {
+    return encrypt_data(self, args, 1);
 }
 
-static PyObject* py_encrypt_file(PyObject* self, PyObject* args) {
-    const char* input_file;
-    const char* output_file;
-    const char* key;
-    int key_len;
+static PyObject *decrypt_metadata(PyObject *self, PyObject *args) {
+    return decrypt_data(self, args, 1);
+}
 
-    if (!PyArg_ParseTuple(args, "ssy#", &input_file, &output_file, &key, &key_len)) {
+static PyObject *encrypt_file(PyObject *self, PyObject *args) {
+    const char *input_filename, *output_filename;
+    unsigned char key[AES_KEY_LENGTH/8];
+    unsigned char iv[AES_BLOCK_SIZE];
+    if (!PyArg_ParseTuple(args, "ssy#y#", &input_filename, &output_filename, &key, sizeof(key), &iv, sizeof(iv))) {
         return NULL;
     }
 
-    FILE* ifp = fopen(input_file, "rb");
-    FILE* ofp = fopen(output_file, "wb");
-    if (!ifp || !ofp) {
+    FILE *input_file = fopen(input_filename, "rb");
+    if (!input_file) {
+        PyErr_SetString(PyExc_FileNotFoundError, "Input file not found");
         return NULL;
     }
 
-    fseek(ifp, 0, SEEK_END);
-    long file_size = ftell(ifp);
-    fseek(ifp, 0, SEEK_SET);
-
-    unsigned char* buffer = (unsigned char*)malloc(file_size);
-    fread(buffer, 1, file_size, ifp);
-
-    unsigned char* ciphertext = (unsigned char*)malloc(file_size + BLOCK_SIZE);
-    if (!encrypt_ige(buffer, file_size, (unsigned char*)key, ciphertext)) {
+    FILE *output_file = fopen(output_filename, "wb");
+    if (!output_file) {
+        fclose(input_file);
+        PyErr_SetString(PyExc_FileNotFoundError, "Could not open output file");
         return NULL;
     }
 
-    fwrite(ciphertext, 1, file_size + BLOCK_SIZE, ofp);
+    AES_KEY aes_key;
+    AES_set_encrypt_key(key, AES_KEY_LENGTH, &aes_key);
 
-    free(buffer);
-    free(ciphertext);
-    fclose(ifp);
-    fclose(ofp);
+    unsigned char buffer[AES_BLOCK_SIZE];
+    unsigned char encrypted[AES_BLOCK_SIZE];
+    size_t bytes_read;
+    while ((bytes_read = fread(buffer, 1, AES_BLOCK_SIZE, input_file)) > 0) {
+        if (bytes_read < AES_BLOCK_SIZE) {
+            memset(buffer + bytes_read, 0, AES_BLOCK_SIZE - bytes_read);
+        }
+        AES_ige_encrypt(buffer, encrypted, AES_BLOCK_SIZE, &aes_key, iv, AES_ENCRYPT);
+        fwrite(encrypted, 1, AES_BLOCK_SIZE, output_file);
+    }
+
+    fclose(input_file);
+    fclose(output_file);
 
     Py_RETURN_NONE;
 }
 
-static PyObject* py_decrypt_file(PyObject* self, PyObject* args) {
-    const char* input_file;
-    const char* output_file;
-    const char* key;
-    int key_len;
-
-    if (!PyArg_ParseTuple(args, "ssy#", &input_file, &output_file, &key, &key_len)) {
+static PyObject *decrypt_file(PyObject *self, PyObject *args) {
+    const char *input_filename, *output_filename;
+    unsigned char key[AES_KEY_LENGTH/8];
+    unsigned char iv[AES_BLOCK_SIZE];
+    if (!PyArg_ParseTuple(args, "ssy#y#", &input_filename, &output_filename, &key, sizeof(key), &iv, sizeof(iv))) {
         return NULL;
     }
 
-    FILE* ifp = fopen(input_file, "rb");
-    FILE* ofp = fopen(output_file, "wb");
-    if (!ifp || !ofp) {
+    FILE *input_file = fopen(input_filename, "rb");
+    if (!input_file) {
+        PyErr_SetString(PyExc_FileNotFoundError, "Input file not found");
         return NULL;
     }
 
-    fseek(ifp, 0, SEEK_END);
-    long file_size = ftell(ifp);
-    fseek(ifp, 0, SEEK_SET);
-
-    unsigned char* buffer = (unsigned char*)malloc(file_size);
-    fread(buffer, 1, file_size, ifp);
-
-    unsigned char* plaintext = (unsigned char*)malloc(file_size);
-    if (!decrypt_ige(buffer, file_size, (unsigned char*)key, plaintext)) {
+    FILE *output_file = fopen(output_filename, "wb");
+    if (!output_file) {
+        fclose(input_file);
+        PyErr_SetString(PyExc_FileNotFoundError, "Could not open output file");
         return NULL;
     }
 
-    fwrite(plaintext, 1, file_size, ofp);
+    AES_KEY aes_key;
+    AES_set_decrypt_key(key, AES_KEY_LENGTH, &aes_key);
 
-    free(buffer);
-    free(plaintext);
-    fclose(ifp);
-    fclose(ofp);
+    unsigned char buffer[AES_BLOCK_SIZE];
+    unsigned char decrypted[AES_BLOCK_SIZE];
+    size_t bytes_read;
+    while ((bytes_read = fread(buffer, 1, AES_BLOCK_SIZE, input_file)) > 0) {
+        AES_ige_encrypt(buffer, decrypted, AES_BLOCK_SIZE, &aes_key, iv, AES_DECRYPT);
+        fwrite(decrypted, 1, bytes_read, output_file);
+    }
+
+    fclose(input_file);
+    fclose(output_file);
 
     Py_RETURN_NONE;
-}
-
-static PyObject* py_encrypt_metadata(PyObject* self, PyObject* args) {
-    const char* metadata;
-    const char* key;
-    int metadata_len, key_len;
-
-    if (!PyArg_ParseTuple(args, "y#y#", &metadata, &metadata_len, &key, &key_len)) {
-        return NULL;
-    }
-
-    unsigned char ciphertext[metadata_len + BLOCK_SIZE];
-    if (!encrypt_ige((unsigned char*)metadata, metadata_len, (unsigned char*)key, ciphertext)) {
-        return NULL;
-    }
-
-    return Py_BuildValue("y#", ciphertext, metadata_len + BLOCK_SIZE);
-}
-
-static PyObject* py_decrypt_metadata(PyObject* self, PyObject* args) {
-    const char* ciphertext;
-    const char* key;
-    int ciphertext_len, key_len;
-
-    if (!PyArg_ParseTuple(args, "y#y#", &ciphertext, &ciphertext_len, &key, &key_len)) {
-        return NULL;
-    }
-
-    unsigned char plaintext[ciphertext_len];
-    if (!decrypt_ige((unsigned char*)ciphertext, ciphertext_len, (unsigned char*)key, plaintext)) {
-        return NULL;
-    }
-
-    return Py_BuildValue("y#", plaintext, ciphertext_len);
 }
 
 static PyMethodDef AESMethods[] = {
-    {"generate_key", py_generate_key, METH_VARARGS, "Generate a 256-bit AES key"},
-    {"encrypt", py_encrypt, METH_VARARGS, "Encrypt text using AES in IGE mode"},
-    {"decrypt", py_decrypt, METH_VARARGS, "Decrypt text using AES in IGE mode"},
-    {"encrypt_file", py_encrypt_file, METH_VARARGS, "Encrypt a file using AES in IGE mode"},
-    {"decrypt_file", py_decrypt_file, METH_VARARGS, "Decrypt a file using AES in IGE mode"},
-    {"encrypt_metadata", py_encrypt_metadata, METH_VARARGS, "Encrypt metadata using AES in IGE mode"},
-    {"decrypt_metadata", py_decrypt_metadata, METH_VARARGS, "Decrypt metadata using AES in IGE mode"},
+    {"generate_key", generate_key, METH_NOARGS, "Generate a 256-bit AES key"},
+    {"encrypt_text", encrypt_text, METH_VARARGS, "Encrypt text using AES IGE"},
+    {"decrypt_text", decrypt_text, METH_VARARGS, "Decrypt text using AES IGE"},
+    {"encrypt_metadata", encrypt_metadata, METH_VARARGS, "Encrypt metadata using AES IGE"},
+    {"decrypt_metadata", decrypt_metadata, METH_VARARGS, "Decrypt metadata using AES IGE"},
+    {"encrypt_file", encrypt_file, METH_VARARGS, "Encrypt a file using AES IGE"},
+    {"decrypt_file", decrypt_file, METH_VARARGS, "Decrypt a file using AES IGE"},
     {NULL, NULL, 0, NULL}
 };
 
